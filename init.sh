07bf6453d6940f49be1d6b6cdde6a371eeaa99d6
leekari <leekari@163.com>
2023-05-31 11:17:50 +0800

    update main

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "start admin"
-#JVM_OPTIONS="-server -Xms512m -Xmx2g"
-#echo "JVM_OPTIONS : "
-#echo "$JVM_OPTIONS"
-
-java -jar -Xms1024m -Xmx2048m /home/jboss/reed-admin.jar
-#tail -f /rflog/REED-ADMIN.log
